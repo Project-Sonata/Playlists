4faaf954f6753244929c1fe4446cb593f8644784
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-11 21:22:40 +0300

    DTO that represent the Playlist

---
+++ src/main/java/com/odeyalo/sonata/playlists/dto/PlaylistDto.java
@@ -0,0 +1,28 @@
+package com.odeyalo.sonata.playlists.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.odeyalo.sonata.playlists.model.EntityType;
+import com.odeyalo.sonata.playlists.model.PlaylistType;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * DTO that represent the Playlist
+ */
+@Data
+@AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PlaylistDto {
+    @JsonProperty("id")
+    String id;
+    @JsonProperty("name")
+    String name;
+    @JsonProperty("description")
+    String description;
+    @JsonProperty("playlist_type")
+    PlaylistType playlistType;
+    @JsonProperty("type")
+    EntityType type;
+}
