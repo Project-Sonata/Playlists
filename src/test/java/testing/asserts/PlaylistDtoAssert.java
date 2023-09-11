5f0af40ce18f3ef413989a61e306c4d8c774303a
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-11 21:23:19 +0300

    Asserts for PlaylistDto class

---
+++ src/test/java/testing/asserts/PlaylistDtoAssert.java
@@ -0,0 +1,43 @@
+package testing.asserts;
+
+import com.odeyalo.sonata.playlists.dto.PlaylistDto;
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.StringAssert;
+
+/**
+ * Asserts for PlaylistDto class
+ */
+public class PlaylistDtoAssert extends AbstractAssert<PlaylistDtoAssert, PlaylistDto> {
+
+    public PlaylistDtoAssert(PlaylistDto actual) {
+        super(actual, PlaylistDtoAssert.class);
+    }
+
+    protected PlaylistDtoAssert(PlaylistDto actual, Class<?> self) {
+        super(actual, self);
+    }
+
+    public static PlaylistDtoAssert forPlaylist(PlaylistDto actual) {
+        return new PlaylistDtoAssert(actual);
+    }
+
+    public IdAssert id() {
+        return new IdAssert(actual.getId());
+    }
+
+    public EntityTypeAssert entityType() {
+        return new EntityTypeAssert(actual.getType());
+    }
+
+    public StringAssert name() {
+        return new StringAssert(actual.getName());
+    }
+
+    public StringAssert description() {
+        return new StringAssert(actual.getDescription());
+    }
+
+    public PlaylistTypeAssert playlistType() {
+        return new PlaylistTypeAssert(actual.getPlaylistType());
+    }
+}
