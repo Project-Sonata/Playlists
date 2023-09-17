7e70d1aeea6fe35b0e761d1b76bf60cf1d186522
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-17 15:42:11 +0300

    SQL script to create playlist table with postgres dialect

---
+++ src/main/resources/db/migration/V1__create_playlist_table.sql
@@ -0,0 +1,11 @@
+--  SQL script to create playlist table with postgres dialect
+CREATE TABLE playlists (
+    playlist_id INTEGER PRIMARY KEY,
+    public_id VARCHAR(22) UNIQUE NOT NULL,
+    playlist_name VARCHAR(150) NOT NULL,
+    playlist_description VARCHAR (400),
+    playlist_type VARCHAR(25) NOT NULL DEFAULT 'PRIVATE'
+);
+
+ALTER TABLE playlists ADD CONSTRAINT playlists
+    CHECK ( playlist_type IN ('PRIVATE', 'PUBLIC', 'COLLABORATIVE') )
