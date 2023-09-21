1a482574af267a2844d59bd70bf89886dedb799b
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-21 22:01:45 +0300

    Added deleteAllByPlaylistId

--- src/main/java/com/odeyalo/sonata/playlists/repository/PlaylistImagesRepository.java
+++ src/main/java/com/odeyalo/sonata/playlists/repository/PlaylistImagesRepository.java
@@ -4,10 +4,12 @@ import com.odeyalo.sonata.playlists.entity.PlaylistImage;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface PlaylistImagesRepository extends ReactiveCrudRepository<PlaylistImage, Long> {
 
     Flux<PlaylistImage> findAllByPlaylistId(Long playlistId);
 
+    Mono<Void> deleteAllByPlaylistId(Long id);
 }
