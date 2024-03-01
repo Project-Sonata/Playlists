8258b5f085af29f058132678e025dc79f806ecdc
odeyalo <theprogrammerinfuture@gmail.com>
2024-03-01 14:36:49 +0200

    Added mappings

--- src/main/java/com/odeyalo/sonata/playlists/support/converter/PlaylistOwnerConverter.java
+++ src/main/java/com/odeyalo/sonata/playlists/support/converter/PlaylistOwnerConverter.java
@@ -1,8 +1,10 @@
 package com.odeyalo.sonata.playlists.support.converter;
 
 import com.odeyalo.sonata.playlists.dto.PlaylistOwnerDto;
+import com.odeyalo.sonata.playlists.entity.PlaylistOwnerEntity;
 import com.odeyalo.sonata.playlists.model.PlaylistOwner;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * Converter for PlaylistOwnerDto
@@ -11,4 +13,11 @@ import org.mapstruct.Mapper;
 public interface PlaylistOwnerConverter {
 
     PlaylistOwnerDto toPlaylistOwnerDto(PlaylistOwner owner);
+
+    @Mapping(target = "id", source = "publicId")
+    PlaylistOwner toPlaylistOwner(PlaylistOwnerEntity entity);
+
+    @Mapping(target = "publicId", source = "id")
+    PlaylistOwnerEntity toPlaylistOwnerEntity(PlaylistOwner owner);
+
 }
