181a2291259298c3dd1efd1c031326e1c575814d
odeyalo <theprogrammerinfuture@gmail.com>
2024-03-06 20:43:59 +0200

    Added new constructors

--- src/main/java/com/odeyalo/sonata/playlists/repository/InMemoryPlaylistRepository.java
+++ src/main/java/com/odeyalo/sonata/playlists/repository/InMemoryPlaylistRepository.java
@@ -5,14 +5,34 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * PlaylistRepository impl that uses Map to store values
  */
 public class InMemoryPlaylistRepository implements PlaylistRepository {
-    private final Map<String, Playlist> playlists = new ConcurrentHashMap<>();
+    private final Map<String, Playlist> playlists;
+
+    public InMemoryPlaylistRepository() {
+        this.playlists = new ConcurrentHashMap<>();
+    }
+
+    public InMemoryPlaylistRepository(Map<String, Playlist> cache) {
+        this.playlists = new ConcurrentHashMap<>(cache);
+    }
+
+    public InMemoryPlaylistRepository(List<Playlist> cache) {
+        Map<String, Playlist> items = cache.stream().collect(Collectors.toMap(Playlist::getId, Function.identity()));
+        this.playlists = new ConcurrentHashMap<>(items);
+    }
+
+    public InMemoryPlaylistRepository(Playlist... cache) {
+        this(List.of(cache));
+    }
 
     @Override
     @NotNull
@@ -42,7 +62,7 @@ public class InMemoryPlaylistRepository implements PlaylistRepository {
     private Playlist updateWithIdOrNothing(Playlist playlist) {
         String id = playlist.getId();
 
-        if (id == null) {
+        if ( id == null ) {
             id = RandomStringUtils.randomAlphanumeric(15);
             playlist = Playlist.from(playlist).id(id).build();
         }
