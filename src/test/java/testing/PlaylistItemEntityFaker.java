eab9f820dfbf2db4d38cd8cf6d32f904c34c81ce
odeyalo <theprogrammerinfuture@gmail.com>
2024-03-06 18:50:04 +0200

    Entity to represent the item(track, podcast, etc) saved in playlist

---
+++ src/test/java/testing/PlaylistItemEntityFaker.java
@@ -0,0 +1,32 @@
+package testing;
+
+import com.github.javafaker.Faker;
+import com.odeyalo.sonata.playlists.entity.ItemEntity;
+import com.odeyalo.sonata.playlists.entity.PlaylistItemEntity;
+
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+public final class PlaylistItemEntityFaker {
+    private final PlaylistItemEntity.PlaylistItemEntityBuilder builder = PlaylistItemEntity.builder();
+    private final Faker faker = Faker.instance();
+
+    public PlaylistItemEntityFaker(Long playlistId) {
+        ItemEntity item = ItemEntityFaker.create().get();
+
+        Instant addedAt = faker.date().past(1, TimeUnit.HOURS).toInstant();
+        builder.addedAt(addedAt)
+                .playlistId(playlistId)
+                .id(faker.random().nextLong(10000))
+                .item(item)
+                .build();
+    }
+
+    public static PlaylistItemEntityFaker create(Long playlistId) {
+        return new PlaylistItemEntityFaker(playlistId);
+    }
+
+    public PlaylistItemEntity get() {
+        return builder.build();
+    }
+}
