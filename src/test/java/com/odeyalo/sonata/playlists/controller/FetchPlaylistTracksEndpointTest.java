e190966d87b4c966fac4bce7e76df938561bd9fd
odeyalo <theprogrammerinfuture@gmail.com>
2024-03-11 22:06:11 +0200

    Controller is using service instead of mocked values now

--- src/test/java/com/odeyalo/sonata/playlists/controller/FetchPlaylistTracksEndpointTest.java
+++ src/test/java/com/odeyalo/sonata/playlists/controller/FetchPlaylistTracksEndpointTest.java
@@ -1,8 +1,21 @@
 package com.odeyalo.sonata.playlists.controller;
 
 
+import com.odeyalo.sonata.playlists.controller.FetchPlaylistTracksEndpointTest.TestConfig;
 import com.odeyalo.sonata.playlists.dto.PlaylistItemDto;
 import com.odeyalo.sonata.playlists.dto.PlaylistItemsDto;
+import com.odeyalo.sonata.playlists.entity.ItemEntity;
+import com.odeyalo.sonata.playlists.entity.PlaylistItemEntity;
+import com.odeyalo.sonata.playlists.model.Playlist;
+import com.odeyalo.sonata.playlists.model.TrackPlayableItem;
+import com.odeyalo.sonata.playlists.repository.InMemoryPlaylistItemsRepository;
+import com.odeyalo.sonata.playlists.repository.InMemoryPlaylistRepository;
+import com.odeyalo.sonata.playlists.repository.PlaylistItemsRepository;
+import com.odeyalo.sonata.playlists.repository.PlaylistRepository;
+import com.odeyalo.sonata.playlists.service.PlaylistLoader;
+import com.odeyalo.sonata.playlists.service.RepositoryDelegatePlaylistLoader;
+import com.odeyalo.sonata.playlists.service.tracks.InMemoryPlayableItemLoader;
+import com.odeyalo.sonata.playlists.service.tracks.PlayableItemLoader;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -10,12 +23,20 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpHeaders;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Hooks;
+import testing.faker.PlaylistFaker;
+import testing.faker.TrackPlayableItemFaker;
+import testing.spring.autoconfigure.AutoConfigureQaEnvironment;
 
+import java.time.Instant;
 import java.util.List;
 
 import static com.odeyalo.sonata.playlists.controller.FetchPlaylistTracksEndpointTest.Limit.limit;
@@ -31,23 +52,62 @@ import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerPro
 @AutoConfigureStubRunner(stubsMode = REMOTE,
         repositoryRoot = "${spring.contracts.repository.root}",
         ids = "com.odeyalo.sonata:authorization:+")
-@TestPropertySource(locations = "classpath:application-test.properties")
+@AutoConfigureQaEnvironment
+@ActiveProfiles("test")
+@Import(TestConfig.class)
 class FetchPlaylistTracksEndpointTest {
-    public static final String TRACK_1_ID = "1";
-    public static final String TRACK_2_ID = "2";
-    public static final String TRACK_3_ID = "3";
     @Autowired
     WebTestClient webTestClient;
-
 
     static final String EXISTING_PLAYLIST_ID = "existingPlaylist";
 
     static final String VALID_ACCESS_TOKEN = "Bearer mikunakanoisthebestgirl";
 
+    static final String TRACK_1_ID = "1";
+    static final String TRACK_2_ID = "2";
+    static final String TRACK_3_ID = "3";
 
     @BeforeAll
     void setup() {
         Hooks.onOperatorDebug(); // DO NOT DELETE IT, VERY IMPORTANT LINE, WITHOUT IT FEIGN WITH WIREMOCK THROWS ILLEGAL STATE EXCEPTION, I DON'T FIND SOLUTION YET
+    }
+
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        @Primary
+        public PlayableItemLoader testPlayableItemLoader() {
+            TrackPlayableItem playableItem = TrackPlayableItemFaker.create().setPublicId(TRACK_1_ID).get();
+            TrackPlayableItem playableItem2 = TrackPlayableItemFaker.create().setPublicId(TRACK_2_ID).get();
+            TrackPlayableItem playableItem3 = TrackPlayableItemFaker.create().setPublicId(TRACK_3_ID).get();
+            return new InMemoryPlayableItemLoader(playableItem, playableItem2, playableItem3);
+        }
+
+        @Bean
+        @Primary
+        public PlaylistLoader testPlaylistLoader(PlaylistRepository playlistRepository) {
+            return new RepositoryDelegatePlaylistLoader(playlistRepository);
+        }
+
+        @Bean
+        @Primary
+        public PlaylistRepository testPlaylistRepository() {
+            Playlist playlist = PlaylistFaker.createWithNoId().setId(EXISTING_PLAYLIST_ID).get();
+            return new InMemoryPlaylistRepository(playlist);
+        }
+
+        @Bean
+        @Primary
+        public PlaylistItemsRepository testPlaylistItemsRepository() {
+            var track = PlaylistItemEntity.of(1L, Instant.now(), ItemEntity.of(1L, TRACK_1_ID,
+                    "sonata:track:" + TRACK_1_ID), EXISTING_PLAYLIST_ID);
+            var track2 = PlaylistItemEntity.of(2L, Instant.now(), ItemEntity.of(2L, TRACK_2_ID,
+                    "sonata:track:" + TRACK_2_ID), EXISTING_PLAYLIST_ID);
+            var track3 = PlaylistItemEntity.of(3L, Instant.now(), ItemEntity.of(3L, TRACK_3_ID,
+                    "sonata:track:" + TRACK_3_ID), EXISTING_PLAYLIST_ID);
+            return new InMemoryPlaylistItemsRepository(List.of(track, track2, track3));
+        }
     }
 
     @Test
