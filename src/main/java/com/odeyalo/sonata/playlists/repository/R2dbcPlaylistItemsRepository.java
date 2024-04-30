4321f2ee47597da1d37a7f53caa9c1eeafea304e
odeyalo <theprogrammerinfuture@gmail.com>
2024-04-30 18:21:39 +0300

    Added test to check if pageable is applied correctly to query method

---
+++ src/main/java/com/odeyalo/sonata/playlists/repository/R2dbcPlaylistItemsRepository.java
@@ -0,0 +1,40 @@
+package com.odeyalo.sonata.playlists.repository;
+
+import com.odeyalo.sonata.playlists.entity.PlaylistItemEntity;
+import com.odeyalo.sonata.playlists.repository.r2dbc.R2dbcPlaylistItemsRepositoryDelegate;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+public final class R2dbcPlaylistItemsRepository implements PlaylistItemsRepository {
+    private final R2dbcPlaylistItemsRepositoryDelegate delegate;
+
+    public R2dbcPlaylistItemsRepository(R2dbcPlaylistItemsRepositoryDelegate delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public @NotNull Flux<PlaylistItemEntity> findAllByPlaylistId(@NotNull String playlistId, @NotNull Pageable pageable) {
+        return delegate.findAllByPlaylistId(playlistId, pageable);
+    }
+
+    @Override
+    public @NotNull Mono<PlaylistItemEntity> save(@NotNull PlaylistItemEntity entity) {
+        return delegate.save(entity);
+    }
+
+    @Override
+    public @NotNull Mono<Void> clear() {
+        return delegate.deleteAll();
+    }
+
+    @NotNull
+    public Flux<PlaylistItemEntity> saveAll(@NotNull List<PlaylistItemEntity> entities) {
+        return delegate.saveAll(entities);
+    }
+}
