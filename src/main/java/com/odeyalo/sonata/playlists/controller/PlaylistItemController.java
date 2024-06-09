4acd1a98f656d2d53eb7f5e66f3c85c7466a69f9
odeyalo <theprogrammerinfuture@gmail.com>
2024-06-09 12:05:19 +0300

    Added hardcoded version of bug fix, when user who is NOT playlist owner can fetch playlist items from playlist

--- src/main/java/com/odeyalo/sonata/playlists/controller/PlaylistItemController.java
+++ src/main/java/com/odeyalo/sonata/playlists/controller/PlaylistItemController.java
@@ -2,8 +2,12 @@ package com.odeyalo.sonata.playlists.controller;
 
 
 import com.odeyalo.sonata.playlists.dto.PlaylistItemsDto;
+import com.odeyalo.sonata.playlists.exception.PlaylistNotFoundException;
+import com.odeyalo.sonata.playlists.exception.PlaylistOperationNotAllowedException;
+import com.odeyalo.sonata.playlists.model.Playlist;
 import com.odeyalo.sonata.playlists.model.PlaylistCollaborator;
 import com.odeyalo.sonata.playlists.model.User;
+import com.odeyalo.sonata.playlists.service.PlaylistLoader;
 import com.odeyalo.sonata.playlists.service.TargetPlaylist;
 import com.odeyalo.sonata.playlists.service.tracks.AddItemPayload;
 import com.odeyalo.sonata.playlists.service.tracks.PlaylistItemsOperations;
@@ -28,14 +32,35 @@ public final class PlaylistItemController {
     private final PlaylistItemsOperations playlistItemsOperations;
     private final PlaylistItemsOperationsFacade playlistItemsOperationsFacade;
     private final PlaylistItemDtoConverter playlistItemDtoConverter;
+    private final PlaylistLoader loader;
 
     @GetMapping(value = "/{playlistId}/items", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<ResponseEntity<PlaylistItemsDto>> fetchPlaylistItems(@PathVariable String playlistId,
-                                                                     Pagination pagination) {
-        return playlistItemsOperations.loadPlaylistItems(TargetPlaylist.just(playlistId), pagination)
+    public Mono<ResponseEntity<PlaylistItemsDto>> fetchPlaylistItems(@PathVariable("playlistId") @NotNull final TargetPlaylist playlist,
+                                                                     @NotNull final Pagination pagination,
+                                                                     @NotNull final User user) {
+        Mono<ResponseEntity<PlaylistItemsDto>> playlistItems = playlistItemsOperations.loadPlaylistItems(playlist, pagination)
                 .map(playlistItemDtoConverter::toPlaylistItemDto)
                 .collectList()
                 .map(items -> defaultOkStatus(new PlaylistItemsDto(items)));
+
+        return loader.loadPlaylist(playlist).switchIfEmpty(onPlaylistNotFoundError(playlist))
+                .flatMap(playlist1 -> {
+                    if ( playlist1.getPlaylistOwner().getId().equals(user.getId()) ) {
+                        return playlistItems;
+                    }
+
+                    return Mono.error(
+                            PlaylistOperationNotAllowedException.defaultException(playlist.getPlaylistId())
+                    );
+                });
+    }
+
+
+    @NotNull
+    private static Mono<Playlist> onPlaylistNotFoundError(@NotNull TargetPlaylist targetPlaylist) {
+        return Mono.defer(
+                () -> Mono.error(PlaylistNotFoundException.defaultException(targetPlaylist.getPlaylistId()))
+        );
     }
 
     @PostMapping(value = "/{playlistId}/items")
