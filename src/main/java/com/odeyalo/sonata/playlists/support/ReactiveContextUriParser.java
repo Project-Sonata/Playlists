f76981f374c32317f8cd6c5498e274839dbdee5f
odeyalo <theprogrammerinfuture@gmail.com>
2024-06-22 23:12:54 +0300

    Bump version of suite-core to 1.0.0. Replaced HardcodedContextUriParser in ReactiveContextUriParser with ContextUri.fromString()

--- src/main/java/com/odeyalo/sonata/playlists/support/ReactiveContextUriParser.java
+++ src/main/java/com/odeyalo/sonata/playlists/support/ReactiveContextUriParser.java
@@ -1,26 +1,21 @@
 package com.odeyalo.sonata.playlists.support;
 
 import com.odeyalo.sonata.common.context.ContextUri;
-import com.odeyalo.sonata.common.context.ContextUriParser;
 import com.odeyalo.sonata.common.context.MalformedContextUriException;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 /**
- * Adapter for {@link ContextUriParser} that can be used for Reactive Streams
+ * Adapter for {@link ContextUri#fromString(String)} that can be used for Reactive Streams
  */
 @Component
 public final class ReactiveContextUriParser {
-    private final ContextUriParser contextUriParser;
 
-    public ReactiveContextUriParser(ContextUriParser contextUriParser) {
-        this.contextUriParser = contextUriParser;
-    }
-
-    public Mono<ContextUri> parse(String contextUriStr) {
+    @NotNull
+    public Mono<ContextUri> parse(@NotNull final String contextUriStr) {
         try {
-            ContextUri contextUri = contextUriParser.parse(contextUriStr);
-            return Mono.just(contextUri);
+            return Mono.just(ContextUri.fromString(contextUriStr));
         } catch (MalformedContextUriException e) {
             return Mono.error(e);
         }
