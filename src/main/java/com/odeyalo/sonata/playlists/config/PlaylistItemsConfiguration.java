160ca5519cf4cf02527395274ff6cd3905329bd5
odeyalo <theprogrammerinfuture@gmail.com>
2024-06-23 20:22:33 +0300

    Replaced RepositoryDelegatePlaylistLoader with PlaylistService in PlaylistItemsConfiguration

--- src/main/java/com/odeyalo/sonata/playlists/config/PlaylistItemsConfiguration.java
+++ src/main/java/com/odeyalo/sonata/playlists/config/PlaylistItemsConfiguration.java
@@ -1,11 +1,10 @@
 package com.odeyalo.sonata.playlists.config;
 
-import com.odeyalo.sonata.playlists.model.PlayableItem;
 import com.odeyalo.sonata.playlists.repository.InMemoryPlaylistItemsRepository;
 import com.odeyalo.sonata.playlists.repository.PlaylistItemsRepository;
 import com.odeyalo.sonata.playlists.repository.PlaylistRepository;
 import com.odeyalo.sonata.playlists.service.PlaylistLoader;
-import com.odeyalo.sonata.playlists.service.RepositoryDelegatePlaylistLoader;
+import com.odeyalo.sonata.playlists.service.PlaylistService;
 import com.odeyalo.sonata.playlists.service.tracks.InMemoryPlayableItemLoader;
 import com.odeyalo.sonata.playlists.service.tracks.PlayableItemLoader;
 import org.springframework.context.annotation.Bean;
@@ -21,7 +20,7 @@ public class PlaylistItemsConfiguration {
 
     @Bean
     public PlaylistLoader playlistLoader(PlaylistRepository playlistRepository) {
-        return new RepositoryDelegatePlaylistLoader(playlistRepository);
+        return new PlaylistService(playlistRepository);
     }
 
     @Bean
