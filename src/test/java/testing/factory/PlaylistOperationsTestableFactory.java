e5e07b16d0f012812dc06f5a8d72b3c8dd1e53fc
odeyalo <theprogrammerinfuture@gmail.com>
2024-06-23 20:40:37 +0300

    Replaced PlaylistRepository to PlaylistService in DefaultPlaylistOperations

--- src/test/java/testing/factory/PlaylistOperationsTestableFactory.java
+++ src/test/java/testing/factory/PlaylistOperationsTestableFactory.java
@@ -4,6 +4,7 @@ import com.odeyalo.sonata.playlists.model.Playlist;
 import com.odeyalo.sonata.playlists.repository.InMemoryPlaylistRepository;
 import com.odeyalo.sonata.playlists.service.DefaultPlaylistOperations;
 import com.odeyalo.sonata.playlists.service.PlaylistOperations;
+import com.odeyalo.sonata.playlists.service.PlaylistService;
 import com.odeyalo.sonata.playlists.service.upload.MockImageUploader;
 
 import java.util.List;
@@ -12,11 +13,11 @@ public class PlaylistOperationsTestableFactory {
 
     public static PlaylistOperations withPlaylists(List<Playlist> playlists) {
         final InMemoryPlaylistRepository repository = new InMemoryPlaylistRepository(playlists);
-        return new DefaultPlaylistOperations(repository, new MockImageUploader());
+        return new DefaultPlaylistOperations(new PlaylistService(repository), new MockImageUploader());
     }
 
     public static DefaultPlaylistOperations create() {
         final InMemoryPlaylistRepository repository = new InMemoryPlaylistRepository();
-        return new DefaultPlaylistOperations(repository, new MockImageUploader());
+        return new DefaultPlaylistOperations(new PlaylistService(repository), new MockImageUploader());
     }
 }
