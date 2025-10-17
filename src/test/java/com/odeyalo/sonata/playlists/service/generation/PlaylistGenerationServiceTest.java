bbe3e0504e74a7b551418566856173162c03b809
odeyalo <theprogrammerinfuture@gmail.com>
2025-10-17 16:40:14 +0300

    Use default account

--- src/test/java/com/odeyalo/sonata/playlists/service/generation/PlaylistGenerationServiceTest.java
+++ src/test/java/com/odeyalo/sonata/playlists/service/generation/PlaylistGenerationServiceTest.java
@@ -1,6 +1,8 @@
 package com.odeyalo.sonata.playlists.service.generation;
 
+import com.odeyalo.sonata.playlists.model.EntityType;
 import com.odeyalo.sonata.playlists.model.Image;
+import com.odeyalo.sonata.playlists.model.PlaylistType;
 import com.odeyalo.sonata.suite.brokers.events.playlist.gen.GeneratedPlaylistType;
 import com.odeyalo.sonata.suite.brokers.events.playlist.gen.PlaylistImagesGeneratedEvent;
 import com.odeyalo.sonata.suite.brokers.events.playlist.gen.payload.GeneratedTrack;
@@ -38,6 +40,8 @@ class PlaylistGenerationServiceTest {
                 .assertNext(playlist -> {
                     assertThat(playlist.getName()).isEqualTo("On Repeat");
                     assertThat(playlist.getDescription()).isEqualTo("Songs you love the most");
+                    assertThat(playlist.getPlaylistType()).isEqualTo(PlaylistType.PUBLIC);
+                    assertThat(playlist.getContextUri().asString()).isEqualTo("sonata:playlist:" + playlist.getId().value());
                 })
                 .verifyComplete();
     }
@@ -70,6 +74,34 @@ class PlaylistGenerationServiceTest {
                             Image.of("https://cdn.sonata.com/i/c/abc124", 350, 300),
                             Image.of("https://cdn.sonata.com/i/c/abc125", 600, 600)
                     );
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldSetThePlaylistOwner() {
+        // given
+        final PlaylistImagesGeneratedEvent event = new PlaylistImagesGeneratedEvent(
+                new PlaylistImagesGeneratedPayload(new PlaylistMetaGeneratedPayload(
+                        new PlaylistTracksGeneratedPayload("123", List.of(
+                                new GeneratedTrack("1", 0),
+                                new GeneratedTrack("2", 1),
+                                new GeneratedTrack("3", 2)
+                        )),
+                        new PlaylistMetaGeneratedPayload.Meta("On Repeat", "Songs you love the most")
+                ), List.of(
+                        new PlaylistImagesGeneratedPayload.Image("https://cdn.sonata.com/i/c/abc123", 50, 50)
+                )), GeneratedPlaylistType.ON_REPEAT);
+
+        final PlaylistGenerationService testable = new PlaylistGenerationService();
+
+        // when
+        testable.generate(event)
+                .as(StepVerifier::create)
+                .assertNext(playlist -> {
+                    assertThat(playlist.getPlaylistOwner().getId()).isEqualTo("sonata");
+                    assertThat(playlist.getPlaylistOwner().getDisplayName()).isEqualTo("Sonata");
+                    assertThat(playlist.getPlaylistOwner().getEntityType()).isEqualTo(EntityType.USER);
                 })
                 .verifyComplete();
     }
