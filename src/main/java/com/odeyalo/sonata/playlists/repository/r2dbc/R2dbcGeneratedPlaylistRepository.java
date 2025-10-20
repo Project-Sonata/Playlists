374f16fdda2f0cc93f06690f778a419f12421d37
odeyalo <theprogrammerinfuture@gmail.com>
2025-10-20 21:45:33 +0300

    Save generated playlist to DB

--- src/main/java/com/odeyalo/sonata/playlists/repository/r2dbc/R2dbcGeneratedPlaylistRepository.java
+++ src/main/java/com/odeyalo/sonata/playlists/repository/r2dbc/R2dbcGeneratedPlaylistRepository.java
@@ -2,6 +2,7 @@ package com.odeyalo.sonata.playlists.repository.r2dbc;
 
 import com.odeyalo.sonata.playlists.entity.GeneratedPlaylistEntity;
 import com.odeyalo.sonata.playlists.repository.GeneratedPlaylistRepository;
+import com.odeyalo.sonata.playlists.repository.r2dbc.delegate.R2dbcGeneratedPlaylistRepositoryDelegate;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
@@ -9,18 +10,27 @@ import reactor.core.publisher.Mono;
 
 @Component
 public final class R2dbcGeneratedPlaylistRepository implements GeneratedPlaylistRepository {
-    @Override
-    public @NotNull Mono<GeneratedPlaylistEntity> save(@NotNull final GeneratedPlaylistEntity playlist) {
-        return null;
+    private final R2dbcGeneratedPlaylistRepositoryDelegate delegate;
+
+    public R2dbcGeneratedPlaylistRepository(final R2dbcGeneratedPlaylistRepositoryDelegate delegate) {
+        this.delegate = delegate;
     }
 
     @Override
-    public @NotNull Flux<GeneratedPlaylistEntity> findByGeneratedFor(@NotNull final String userId) {
-        return null;
+    @NotNull
+    public Mono<GeneratedPlaylistEntity> save(@NotNull final GeneratedPlaylistEntity playlist) {
+        return delegate.save(playlist);
     }
 
     @Override
-    public @NotNull Mono<Void> clear() {
-        return null;
+    @NotNull
+    public Flux<GeneratedPlaylistEntity> findByGeneratedFor(@NotNull final String userId) {
+        return delegate.findByUserId(userId);
+    }
+
+    @Override
+    @NotNull
+    public Mono<Void> clear() {
+        return delegate.deleteAll();
     }
 }
--- src/main/java/com/odeyalo/sonata/playlists/service/generation/PlaylistGenerationManager.java
+++ src/main/java/com/odeyalo/sonata/playlists/service/generation/PlaylistGenerationManager.java
@@ -1,10 +1,9 @@
 package com.odeyalo.sonata.playlists.service.generation;
 
 import com.odeyalo.sonata.common.context.ContextUri;
-import com.odeyalo.sonata.playlists.model.EntityType;
-import com.odeyalo.sonata.playlists.model.PlaylistCollaborator;
-import com.odeyalo.sonata.playlists.model.PlaylistItemPosition;
-import com.odeyalo.sonata.playlists.model.SimplePlaylistItem;
+import com.odeyalo.sonata.playlists.entity.GeneratedPlaylistEntity;
+import com.odeyalo.sonata.playlists.model.*;
+import com.odeyalo.sonata.playlists.repository.GeneratedPlaylistRepository;
 import com.odeyalo.sonata.playlists.service.PlaylistService;
 import com.odeyalo.sonata.playlists.service.tracks.PlaylistItemsService;
 import com.odeyalo.sonata.suite.brokers.events.playlist.gen.PlaylistImagesGeneratedEvent;
@@ -17,6 +16,7 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 
+import java.time.Instant;
 import java.util.List;
 
 @Service
@@ -24,6 +24,8 @@ public class PlaylistGenerationManager {
     private final PlaylistGenerationService generationService;
     private final PlaylistItemsService playlistItemsService;
     private final PlaylistService playlistService;
+    private final GeneratedPlaylistRepository generatedPlaylistRepository;
+
     private final Logger logger = LoggerFactory.getLogger(PlaylistGenerationManager.class);
 
     private static final PlaylistCollaborator SONATA_COLLABORATOR = PlaylistCollaborator.builder()
@@ -35,10 +37,11 @@ public class PlaylistGenerationManager {
 
     public PlaylistGenerationManager(final PlaylistGenerationService generationService,
                                      final PlaylistService playlistService,
-                                     final PlaylistItemsService playlistItemsService) {
+                                     final PlaylistItemsService playlistItemsService, final GeneratedPlaylistRepository generatedPlaylistRepository) {
         this.generationService = generationService;
         this.playlistItemsService = playlistItemsService;
         this.playlistService = playlistService;
+        this.generatedPlaylistRepository = generatedPlaylistRepository;
     }
 
     @NotNull
@@ -48,20 +51,35 @@ public class PlaylistGenerationManager {
         logger.info("Starting playlist generation for user {} with type: {}", parentEvent.getUserId(), event.getType());
 
         return generationService.generate(event)
-            .flatMap(generatedPlaylist -> {
-                return playlistService.save(generatedPlaylist.meta())
-                        .flatMap(playlist -> {
-                            List<GeneratedPlaylist.Item> tracks = generatedPlaylist.tracks();
+                .flatMap(generatedPlaylist -> {
+                    return playlistService.save(generatedPlaylist.meta())
+                            .flatMap(playlist -> {
+                                final List<SimplePlaylistItem> items = getTracks(generatedPlaylist, playlist);
 
-                            List<SimplePlaylistItem> items = tracks.stream().map(item -> new SimplePlaylistItem(
-                                    playlist.getId(),
-                                    SONATA_COLLABORATOR,
-                                    ContextUri.forTrack(item.id()),
-                                    PlaylistItemPosition.at(item.index())
-                            )).toList();
+                                return playlistItemsService.insertAll(items)
+                                        .then(Mono.defer(() -> generatedPlaylistRepository.save(
+                                                GeneratedPlaylistEntity.builder()
+                                                        .userId(event.getBody().getParent().getParent().getUserId())
+                                                        .playlistId(playlist.getId().value())
+                                                        .generatedAt(Instant.now())
+                                                        .generatedPlaylistType(event.getType())
+                                                        .build()
+                                        )))
+                                        .then();
+                            });
+                });
+    }
 
-                            return playlistItemsService.insertAll(items);
-                        });
-            });
+    @NotNull
+    private static List<SimplePlaylistItem> getTracks(final GeneratedPlaylist generatedPlaylist,
+                                                      final Playlist playlist) {
+        List<GeneratedPlaylist.Item> tracks = generatedPlaylist.tracks();
+
+        return tracks.stream().map(item -> new SimplePlaylistItem(
+                playlist.getId(),
+                SONATA_COLLABORATOR,
+                ContextUri.forTrack(item.id()),
+                PlaylistItemPosition.at(item.index())
+        )).toList();
     }
 }
